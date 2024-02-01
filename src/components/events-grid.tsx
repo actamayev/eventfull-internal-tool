4ce15a34fb67f7a971e77b6e5bf655279b53f882
atamayev <75034340+atamayev@users.noreply.github.com>
2024-02-01 18:16:12 -0500

    Events UI improvements

--- src/components/events-grid.tsx
+++ src/components/events-grid.tsx
@@ -3,15 +3,19 @@ import { autorun, toJS } from "mobx"
 import { observer } from "mobx-react"
 import { AgGridReact } from "ag-grid-react"
 import "ag-grid-community/styles/ag-grid.css"
+import { useNavigate } from "react-router-dom"
 import "ag-grid-community/styles/ag-theme-alpine.css"
-import { useState, useEffect, useContext } from "react"
+import { useState, useEffect, useContext, useRef, useCallback } from "react"
 import { GridApi, GridReadyEvent, SizeColumnsToContentStrategy  } from "ag-grid-community"
+import Button from "./button"
 import AppContext from "../contexts/eventfull-it-context"
 import dashboardColumns from "../utils/events/dashboard-colums"
 import createEventsArrayForGrid from "../utils/events/create-events-array-for-grid"
 
 function EventsGrid () {
 	const appContext = useContext(AppContext)
+	const gridRef = useRef<AgGridReact | null>(null)
+	const navigate = useNavigate()
 	const [rowData, setRowData] = useState<GridRowData[]>([])
 	const [gridApi, setGridApi] = useState<GridApi | null>(null)
 
@@ -40,21 +44,51 @@ function EventsGrid () {
 		// Additional logic for when the grid is ready
 	}
 
-	const autoSizeStrategy: SizeColumnsToContentStrategy  = {
+	const autoSizeStrategy: SizeColumnsToContentStrategy = {
 		type: "fitCellContents",
 	}
 
+	const onFilterTextBoxChanged = useCallback(() => {
+		if (!gridRef.current) return
+
+		const filterText = (document.getElementById("filter-text-box") as HTMLInputElement).value
+		gridRef.current.api.updateGridOptions({ quickFilterText: filterText })
+	}, [gridRef])
+
 	return (
-		<div className="ag-theme-alpine" style={{ height: 900, width: "100%" }}>
-			<AgGridReact
-				columnDefs={dashboardColumns}
-				rowData={rowData}
-				onGridReady={onGridReady}
-				pagination={true}
-				paginationPageSize={25}
-				rowHeight={30}
-				autoSizeStrategy={autoSizeStrategy}
-			/>
+		<div className="flex-grow">
+			<div className="flex justify-between mb-4">
+				<div className="flex-grow">
+					<input
+						type="text"
+						id="filter-text-box"
+						placeholder="Search Events..."
+						onInput={onFilterTextBoxChanged}
+						className="p-2 border-2 border-gray-300 rounded-md w-1/6"
+					/>
+				</div>
+				<div>
+					<Button
+						title="+ Add event"
+						onClick={() => navigate("/add-event")}
+						colorClass="bg-blue-400"
+						hoverClass="hover:bg-blue-500"
+						className="p-2 rounded-md font-bold"
+					/>
+				</div>
+			</div>
+			<div className="ag-theme-alpine" style={{ height: 900, width: "100%" }}>
+				<AgGridReact
+					ref={gridRef}
+					columnDefs={dashboardColumns}
+					rowData={rowData}
+					onGridReady={onGridReady}
+					pagination={true}
+					paginationPageSize={25}
+					rowHeight={30}
+					autoSizeStrategy={autoSizeStrategy}
+				/>
+			</div>
 		</div>
 	)
 }
