6c652fbf9b3fd241e025f21240421a638bd76402
atamayev <75034340+atamayev@users.noreply.github.com>
2024-02-05 09:43:21 -0500

    Upload multiple images when creating event

--- src/components/image-uploader.tsx
+++ src/components/image-uploader.tsx
@@ -1,27 +1,42 @@
-import { useState, ChangeEvent } from "react"
+import { useState, ChangeEvent, useRef } from "react"
+import Button from "./button"
+import _ from "lodash"
 
 interface Props {
 	selectedFiles: File[]
 	setSelectedFiles: (files: File[]) => void
 }
 
-export default function ImageUploader (props: Props) {
+export default function ImageUploader(props: Props) {
 	const { selectedFiles, setSelectedFiles } = props
 	const [previewUrls, setPreviewUrls] = useState<string[]>([])
+	const fileInputRef = useRef<HTMLInputElement>(null)
 
 	const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
 		if (e.target.files && e.target.files.length > 0) {
-			const files = Array.from(e.target.files)
-			setSelectedFiles(files)
+			const newFiles = Array.from(e.target.files)
+			const totalFiles = selectedFiles.concat(newFiles).slice(0, 10) // Limit to 10 images
+			setSelectedFiles(totalFiles)
 
-			const newPreviewUrls = files.map(file => URL.createObjectURL(file))
+			const newPreviewUrls = totalFiles.map(file => URL.createObjectURL(file))
 			setPreviewUrls(newPreviewUrls)
 		}
+		if (_.isNull(fileInputRef.current)) return
+		fileInputRef.current.value = ""
+	}
+
+	const removeImage = (index: number) => {
+		const updatedFiles = selectedFiles.filter((a, idx) => idx !== index)
+		setSelectedFiles(updatedFiles)
+
+		const updatedPreviewUrls = updatedFiles.map(file => URL.createObjectURL(file))
+		setPreviewUrls(updatedPreviewUrls)
 	}
 
 	return (
 		<div className="mb-2">
 			<input
+				ref={fileInputRef}
 				type="file"
 				onChange={handleImageChange}
 				multiple
@@ -29,9 +44,22 @@ export default function ImageUploader (props: Props) {
 			/>
 			<div className="preview-container">
 				{previewUrls.map((url, index) => (
-					<img key={index} src={url} alt={`Preview ${index}`} style={{ width: "100px", height: "100px" }} />
+					<div key={index} style={{ position: "relative", display: "inline-block", margin: "5px" }}>
+						<img
+							src={url}
+							alt={`Preview ${index}`}
+							style={{ maxWidth: "35%", height: "auto" }}
+						/>
+						<Button
+							title="Remove"
+							colorClass="bg-red-500"
+							hoverClass="hover:bg-red-600"
+							onClick={() => removeImage(index)}
+						/>
+					</div>
 				))}
 			</div>
 		</div>
 	)
 }
+
